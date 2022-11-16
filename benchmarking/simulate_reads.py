64f26622fb94e9ab92f715c78220610cb337f757
Dreycey <albindreycey@gmail.com>
2022-11-15 23:22:18 -0700

    adding updated read simulator script

--- benchmarking/simulate_reads.py
+++ benchmarking/simulate_reads.py
@@ -4,10 +4,10 @@ Description:
     a given genome (in fasta format).
 
 Usage:
-    python <fasta genome path> <number of reads/substrings> <output file>
+    python benchmarking/simulate_reads.py <fasta genome path> <number of reads/substrings> <output file> <read length>
 
 Example:
-    python src/simualte_reads.py genomes/GCF_000912255.1_ViralProj226726_genomic.fna 1000 simulated_reads.fa
+    python benchmarking/simulate_reads.py genomes/GCF_000912255.1_ViralProj226726_genomic.fna 1000 simulated_reads.fa 100
 """
 import sys
 import random 
@@ -26,11 +26,13 @@ def parse_fasta(file_name):
         while line:
             if count > 0:
                 genome += line.strip("\n")
+            else:
+                name = line.strip(">").strip("\n")
             line = f.readline()
             count += 1
-    return genome
+    return genome, name
             
-def simulate_reads(genome, read_count, outfile, readlength=100):
+def simulate_reads(genome, name, read_count, outfile, readlength=100):
     """
     This does not actually simulate reads, as
     it creates a fasta of perfect substrings.
@@ -42,13 +44,14 @@ def simulate_reads(genome, read_count, outfile, readlength=100):
             start = random.randint(0, len(genome)-1)
             stop = start + readlength
             read = genome[start:stop]
-            out.write(f">read_{reads_added}\n{read}\n")
+            out.write(f">{name}_{reads_added}\n{read}\n")
             reads_added += 1
 
 if __name__ == "__main__":
     # parse arguments
-    genome = parse_fasta(sys.argv[1])
+    genome, name = parse_fasta(sys.argv[1])
     read_count = int(sys.argv[2])
     outfile = sys.argv[3]
+    read_length = int(sys.argv[4])
     # simulate reads
-    simulate_reads(genome, read_count, outfile)
+    simulate_reads(genome, name, read_count, outfile, readlength=read_length)
